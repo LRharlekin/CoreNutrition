0ca999f08e7c7327a0bde9b23103052906615117
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-05-18 23:15:05 +0200

    query validator for GetCategoryById added

---
+++ src/CoreNutrition.Application/Category/Queries/GetCategoryById/GetCategoryByIdQueryValidator.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using ErrorOr;
+using FluentValidation;
+
+namespace CoreNutrition.Application.Categories.Queries.GetCategoryById;
+
+public class GetCategoryByIdQuerydValidator
+  : AbstractValidator<GetCategoryByIdQuery>
+{
+  public GetCategoryByIdQuerydValidator()
+  {
+    RuleFor(x => x.Id.Value.ToString())
+      .Length(36)
+      .Matches(@"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+  }
+}
