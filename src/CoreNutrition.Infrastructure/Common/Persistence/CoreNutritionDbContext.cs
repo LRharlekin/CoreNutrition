07ddb14364abb9e639e6af06b693f5ac95b08126
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-07-26 10:39:04 +0300

    EF + db setup

---
+++ src/CoreNutrition.Infrastructure/Common/Persistence/CoreNutritionDbContext.cs
@@ -0,0 +1,9 @@
+namespace CoreNutrition.Infrastructure.Common.Persistence;
+
+public class CoreNutritionDbContext
+{
+  public CoreNutritionDbContext(DbContextOptions<CoreNutritionDbContext> options)
+  {
+    // TODO: Add DbContext
+  }
+}
