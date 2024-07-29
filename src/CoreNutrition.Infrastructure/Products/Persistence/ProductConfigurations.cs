67d1a9dad8871ea9291a0f0f2394d3e0f72cd536
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-07-29 08:38:36 +0300

    Product: entity mapping configs continued

--- src/CoreNutrition.Infrastructure/Products/Persistence/ProductConfigurations.cs
+++ src/CoreNutrition.Infrastructure/Products/Persistence/ProductConfigurations.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 using CoreNutrition.Domain.ProductAggregate;
+using CoreNutrition.Domain.ProductAggregate.ValueObjects;
+using CoreNutrition.Domain.ProductLineAggregate.ValueObjects;
+using CoreNutrition.Domain.ProductLineSizeAggregate.ValueObjects;
+using CoreNutrition.Domain.ProductLineFlavourAggregate.ValueObjects;
 
 namespace CoreNutrition.Infrastructure.Products.Persistence;
 
@@ -26,5 +30,39 @@ public class ProductConfigurations : IEntityTypeConfiguration<Product>
     builder.ToTable("Products");
 
     builder.HasKey(p => p.Id);
+
+    builder.Property(p => p.Id)
+      .ValueGeneratedNever() // EF Core tries to generate ID in db by default
+      .HasConversion(
+        id => id.Value,
+        value => ProductId.Create(value));
+
+    builder.Property(p => p.Name)
+      .HasMaxLength(Product.Constraints.MaxNameLength);
+
+    
+
+    builder.OwnsOne(p => p.AverageRating);
+
+    builder.OwnsOne(p => p.RetailPrice);
+
+    // FKs
+    builder.Property(p => p.ProductLineId)
+      .HasConversion(
+        id => id.Value,
+        value => ProductLineId.Create(value)
+      );
+
+    builder.Property(p => p.ProductLineSizeId)
+      .HasConversion(
+        id => id.Value,
+        value => ProductLineSizeId.Create(value)
+      );
+      
+    builder.Property(p => p.ProductLineFlavourId)
+      .HasConversion(
+        id => id.Value,
+        value => ProductLineFlavourId.Create(value)
+      );
   }
 }
