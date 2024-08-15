0a41d47d6068c67e635b2869026a09c67275fc09
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-08-15 16:40:47 +0300

    async repo methods

--- src/CoreNutrition.Infrastructure/Categories/Persistence/CategoryRepository.cs
+++ src/CoreNutrition.Infrastructure/Categories/Persistence/CategoryRepository.cs
@@ -7,11 +7,21 @@ namespace CoreNutrition.Infrastructure.Categories.Persistence;
 public class CategoryRepository : ICategoryRepository
 {
   private static readonly List<Category> _categories = new();
+  // private readonly CoreNutritionDbContext _dbContext;
+  // public CategoryRepository(CoreNutritionDbContext dbContext)
+  // {
+  //   _dbContext = dbContext;
+  // }
 
   public void Add(Category category)
   {
     _categories.Add(category);
   }
+  // public async Task AddAsync(Category category)
+  // {
+  //   _dbContext.Add(category);
+  //   await _dbContext.SaveChangesAsync();
+  // }
 
   // update
 
